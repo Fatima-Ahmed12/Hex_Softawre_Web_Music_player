13ddfd915b322e98656046873364645b9be44f6e
Fatima-Ahmed12 <fatima123ahemd@gmail.com>
2026-05-13 12:30:01 -0700

    add new Files

---
+++ js/Music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+{
+    name:"Carnaval",
+    artist:"Alec_Koff",
+    img:"Music1",
+    src:"Music1",
+},
+    {
+    name:"Escape your love",
+    artist:"FASS",
+    img:"Music2",
+    src:"Music2",
+},
+{
+    name:"Water",
+    artist:"kontraa",
+    img:"Music3",
+    src:"Music3",
+},
+{
+    name:"Dance Playful Night",
+    artist:"AleXZavesa",
+    img:"Music4",
+    src:"Music4",
+},
+{
+    name:"Powerful Dramatic Trailer",
+    artist:"ArtMyLife",
+    img:"Music5",
+    src:"Music5",
+},
+{
+    name:"Medicine",
+    artist:"Gvidon",
+    img:"Music6",
+    src:"Music6",
+},
+]
